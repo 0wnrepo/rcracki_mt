e5ad4e608e4f138cdf676f5b9c675ab40c79c0e4
quelrod <quelrod@d3517bff-0f9d-46f7-b303-814026e826cd>
2009-10-19 09:30:36 +0000

    follow the method used in sha1.cpp and fast_md5.cpp
    git-svn-id: svn://svn.code.sf.net/p/rcracki/code@36 d3517bff-0f9d-46f7-b303-814026e826cd

--- rcracki_mt/md4.h
+++ rcracki_mt/md4.h
@@ -1,12 +1,7 @@
 #ifndef MD4_H
 #define MD4_H
 
-//typedef unsigned long uint32;
-#if defined(_WIN64) || defined(_LP64)
 typedef unsigned int UINT4;
-#else
-typedef unsigned long UINT4;
-#endif
 
 //Main function
 void MD4_NEW( unsigned char * buf, int len, unsigned char * pDigest);

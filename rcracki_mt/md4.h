6ce2e493e0cedd3d66371e6dcab1268716eb956c
quelrod <quelrod@d3517bff-0f9d-46f7-b303-814026e826cd>
2009-10-19 01:32:19 +0000

    unsigned long on 64bit platforms is 8 bytes force using unsigned int for UINT4 so it is actually 4 bytes this probably has wider impact than just ntlm on 64bit being broken prior to this fix
    git-svn-id: svn://svn.code.sf.net/p/rcracki/code@35 d3517bff-0f9d-46f7-b303-814026e826cd

--- rcracki_mt/md4.h
+++ rcracki_mt/md4.h
@@ -2,7 +2,11 @@
 #define MD4_H
 
 //typedef unsigned long uint32;
+#if defined(_WIN64) || defined(_LP64)
+typedef unsigned int UINT4;
+#else
 typedef unsigned long UINT4;
+#endif
 
 //Main function
 void MD4_NEW( unsigned char * buf, int len, unsigned char * pDigest);
